cecfaee98827f283abdcaf19ab8c8ebbfee8baf3
Erik Öberg <eriobe@miun.se>
2024-09-03 10:24:52 +0200

    Nu har vi ett command som lägger till nya prospects i klubben

--- grunderMVVM/ViewModels/MainViewModel.cs
+++ grunderMVVM/ViewModels/MainViewModel.cs
@@ -6,37 +6,39 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PropertyChanged;
+using System.Windows.Input;
+using grunderMVVM.Commands;
 
 namespace grunderMVVM.ViewModels
 {
-    public class MainViewModel : INotifyPropertyChanged
+    [AddINotifyPropertyChangedInterface]
+    public class MainViewModel
     {
         // Måste binda mot en property!
-        //public ICanFly SelectedFlyingObject { get; set; }
+        public ICanFly SelectedFlyingObject { get; set; }
+        public ICommand AddObjectCommand { get;}
+
+        private List<ICanFly> _prospects = new List<ICanFly>();
         // backing fields
-        private ICanFly _selectedFlyingObject;
+        //private ICanFly _selectedFlyingObject;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
 
-        public ICanFly SelectedFlyingObject
-        {
-            get { return _selectedFlyingObject; }
-            set 
-            {
-                if (_selectedFlyingObject != value)
-                {
-                    _selectedFlyingObject = value; 
-                    OnPropertyChanged(nameof(SelectedFlyingObject));
-                    CheckObject(_selectedFlyingObject);
-                }    
+        //public ICanFly SelectedFlyingObject
+        //{
+        //    get { return _selectedFlyingObject; }
+        //    set 
+        //    {
+        //        if (_selectedFlyingObject != value)
+        //        {
+        //            _selectedFlyingObject = value; 
+        //            OnPropertyChanged(nameof(SelectedFlyingObject));
+        //            CheckObject(_selectedFlyingObject);
+        //        }    
 
-            }
-        }
+        //    }
+        //}
 
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
 
         private void CheckObject(ICanFly selectedFlyingObject)
         {
@@ -54,7 +56,15 @@ namespace grunderMVVM.ViewModels
            // skapa ett nytt 
            // lägg i listan
         }
-
+        private void AddObject()
+        {
+            var prospect = _prospects.FirstOrDefault();
+            if (prospect != null)
+            {
+                FlyingObjects.Add(prospect);
+                _prospects.Remove(prospect);
+            }
+        }
         public ObservableCollection<ICanFly> FlyingObjects { get; set; } = new ObservableCollection<ICanFly>();
         public MainViewModel()
         {
@@ -62,6 +72,9 @@ namespace grunderMVVM.ViewModels
             var airplane = new Airplane();
             FlyingObjects.Add(bird);
             FlyingObjects.Add(airplane);
+            var prospect = new Bird();
+            _prospects.Add(prospect);
+            AddObjectCommand = new RelayCommand(AddObject);
         }
     }
 }
