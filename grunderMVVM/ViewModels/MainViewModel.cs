770bbd21913ce5a6acb1d1fd2d4c72bb25451416
flis4n <lisa.sallvin@gmail.com>
2024-09-05 09:51:09 +0200

    Update MainViewModel.cs
    #FL-7

--- grunderMVVM/ViewModels/MainViewModel.cs
+++ grunderMVVM/ViewModels/MainViewModel.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using PropertyChanged;
 using System.Windows.Input;
 using grunderMVVM.Commands;
-
+//test
 namespace grunderMVVM.ViewModels
 {
     [AddINotifyPropertyChangedInterface]
